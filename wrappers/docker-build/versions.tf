d8810bdc21b16c7bef1e456b640faf3c7fbdbe07
Eunice Atieno <eunice@Eunices-Macbook.local>
2025-06-29 22:32:05 +0300

    play with hashicorp/aws versions

--- wrappers/docker-build/versions.tf
+++ wrappers/docker-build/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6.0"
+      version = "~> 5.43"
     }
     docker = {
       source  = "kreuzwerker/docker"
